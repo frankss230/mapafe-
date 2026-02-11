27358fc0996ce4b53f99b86b6889a41ad263f8b9
frankss230 <frankss230@gmail.com>
2026-02-11 18:27:13 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        'slide-up': {
+          '0%': { transform: 'translateY(100%)' },
+          '100%': { transform: 'translateY(0)' },
+        }
+      },
+      animation: {
+        'slide-up': 'slide-up 0.5s ease-out',
+      }
+    },
+  },
+  plugins: [],
+}
+
+// sdsdsdsd
